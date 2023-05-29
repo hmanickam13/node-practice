d85aab2dd2e3a537a57b9e720229abcb864782a4
hmanickam13 <hmanickam13@gmail.com>
2023-05-29 12:31:25 -0500

    renamed first file

--- app.js
+++
@@ -1,9 +0,0 @@
-// Simple function
-function sayHello(name) {
-    console.log('Hello ' + name);
-}
-
-sayHello('Hariharan');
-
-// node does not have window object like browsers.
-// console.log(window)
